18f53a5664cc17e048b2dbe46fdd2efa2c8dc93d
Aysun Ismayilova <147844360+aysun013@users.noreply.github.com>
2023-11-22 12:56:38 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+let start = prompt("Baslangic reqemini daxil edin")
+let end = prompt("Bitis reqemini daxil et")
+
+let baslangicQiymet = start;
+
+while ( baslangicQiymet <= end){
+    if (baslangicQiymet % 2 === 0){
+    console. log(baslangicQiymet);
+    }
+    baslangicQiymet++;
+}
+alert(son qiymet baslangic qiymetden kicik ola bilmez)
